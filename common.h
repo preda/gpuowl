ce7c18e6986d5afd026bc1dee65fc5fc450a8f6b
Mihai Preda <mhpreda@gmail.com>
2017-11-06 13:56:14 +1100

    typedef uint & ulong on C++ side

--- common.h
+++ common.h
@@ -9,8 +9,12 @@ typedef unsigned long long u64;
 typedef int      i32;
 typedef unsigned u32;
 
-static_assert(sizeof(u32) == 4, "size u32");
-static_assert(sizeof(u64) == 8, "size u64");
+typedef unsigned int uint;
+typedef unsigned long ulong;
+
+static_assert(sizeof(u32) == 4,   "size u32");
+static_assert(sizeof(u64) == 8,   "size u64");
+static_assert(sizeof(ulong) == 8, "size ulong");
 
 #ifdef __GNUC__
 void log(const char *fmt, ...) __attribute__ ((format(printf, 1, 2)));
