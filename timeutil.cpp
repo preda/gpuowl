1d63ee6f7235a3e6562847642c96f490a562312e
Mihai Preda <mhpreda@gmail.com>
2018-09-19 00:01:40 +1000

    add timeutil.cpp

---
+++ timeutil.cpp
@@ -0,0 +1,19 @@
+// GpuOwl Mersenne primality tester; Copyright (C) 2017-2018 Mihai Preda.
+
+#include "timeutil.h"
+
+#include <ctime>
+
+std::string timeStr(const char *format) {
+  time_t t = time(NULL);
+  char buf[64];
+  strftime(buf, sizeof(buf), format, localtime(&t));
+  return buf;
+}
+
+std::string timeStr() {
+  time_t t = time(NULL);
+  char buf[64];
+  strftime(buf, sizeof(buf), "%Y-%m-%d %H:%M:%S UTC", gmtime(&t));   // equivalent to: "%F %T"
+  return buf;
+}
