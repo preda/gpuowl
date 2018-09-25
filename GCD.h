8d46dddd86d5ded38f5c55ea84dac09cdbd01c73
Mihai Preda <mhpreda@gmail.com>
2018-09-25 18:12:08 +1000

    add GCD

---
+++ GCD.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "common.h"
+
+#include <future>
+#include <string>
+#include <chrono>
+#include <vector>
+
+class GCD {
+  future<string> gcdFuture;
+  
+public:
+  void start(u32 E, const vector<u32> &bits, u32 sub);
+  bool isOngoing() { return gcdFuture.valid(); }
+  bool isReady() { return isOngoing() && gcdFuture.wait_for(chrono::steady_clock::duration::zero()) == future_status::ready; }      
+  string get() { return gcdFuture.get(); }
+  void wait() { gcdFuture.wait(); }
+};
