fc3a6edbad74977c973c6ebfef5f35c89b63de54
valeriob01 <valerio@selnet.org>
2020-07-24 08:10:51 +0200

    copy proof files to a backup location.
    Also copy proof files to a backup location if specified.

--- tools/copy_proofs.sh
+++ tools/copy_proofs.sh
@@ -2,4 +2,9 @@
 
 set -o xtrace
 
+# Copy files to upload location.
 find $1 -name '*.proof' -exec cp -v -n '{}' $2 \;
+
+# Also copy to a backup location.
+ [[ ! -z "$3" ]] && find $1 -name '*.proof' -exec cp -v -n '{}' $3 \; || echo "No backup location specified, proof files still copied to upload location."
+ 
