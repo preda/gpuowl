da5f482340da9566f14f93669fb52accec6a85b7
Valerio Bellizzomi <55827668+selroc@users.noreply.github.com>
2020-07-25 05:31:35 +0200

    Remove unnecessary statements.

--- tools/copy_proofs.sh
+++ tools/copy_proofs.sh
@@ -5,9 +5,9 @@ if [[ $# -eq 2 ]]; then
     echo "Cannot copy to the same directory."; exit;
   fi
   # Copy files to backup location.
-  find $1 -name '*.proof' -exec cp -v -n '{}' $2 \; exit;
+  find $1 -name '*.proof' -exec cp -v -n '{}' $2 \;
 elif [[ $# -lt 2 ]]; then
-  echo "Too few arguments. Must specify original and destination directories."; exit;
+  echo "Too few arguments. Must specify original and destination directories.";
 elif [[ $# -gt 2 ]]; then
-  echo "Too much arguments. Must specify original and destination directories only."; exit;
+  echo "Too much arguments. Must specify original and destination directories only.";
 fi
