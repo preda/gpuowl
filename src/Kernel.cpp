abfd8d052b78a4d78ac158cd39be8cd10fc42e0b
Mihai Preda <mhpreda@gmail.com>
2024-03-03 16:05:34 +0200

    CI minor fixes

--- src/Kernel.cpp
+++ src/Kernel.cpp
@@ -5,6 +5,6 @@ void Kernel::load(const KernelCompiler& compiler, cl_device_id deviceId) {
   assert(!kernel);
   kernel = compiler.load(fileName, nameInFile, defines);
   assert(kernel);
-  u32 gs = getWorkGroupSize(kernel.get(), deviceId, name.c_str());
+  [[maybe_unused]] u32 gs = getWorkGroupSize(kernel.get(), deviceId, name.c_str());
   assert(gs == groupSize);
 }
