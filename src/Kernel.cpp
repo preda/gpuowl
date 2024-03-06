237c201e7f8c4bd36928ea43f3a69c8720b04fc5
Mihai Preda <mhpreda@gmail.com>
2024-03-06 15:25:47 +0200

    Kernel class minor cleanup

--- src/Kernel.cpp
+++ src/Kernel.cpp
@@ -1,5 +1,20 @@
-#include "kernel.h"
+// Copyright (C) Mihai Preda
+
+#include "Kernel.h"
 #include "KernelCompiler.h"
+
+#include <stdexcept>
+
+Kernel::Kernel(string_view name, QueuePtr queue,
+       string_view fileName, string_view nameInFile,
+       size_t workSize, string_view defines):
+  name{name},
+  fileName{fileName},
+  nameInFile{nameInFile},
+  defines{defines},
+  queue{queue},
+  workSize{workSize}
+{}
 
 void Kernel::load(const KernelCompiler& compiler, cl_device_id deviceId) {
   assert(!kernel);
@@ -8,4 +23,12 @@ void Kernel::load(const KernelCompiler& compiler, cl_device_id deviceId) {
   groupSize = getWorkGroupSize(kernel.get(), deviceId, name.c_str());
   assert(groupSize);
   assert(workSize % groupSize == 0);
+}
+
+void Kernel::run() {
+  if (kernel) {
+    queue->run(kernel.get(), groupSize, workSize, name);
+  } else {
+    throw std::runtime_error("OpenCL kernel "s + name + " not found");
+  }
 }
